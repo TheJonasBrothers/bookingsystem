e4a70ab9c9f8eb8d480c01c4b2aae2bc21908114
Jonas Andersen <hvidovrefifa@gmail.com>
2026-05-09 23:24:10 +0200

    feat: DB connector class version 1

---
+++ src/main/java/org/example/db/DBConnection.java
@@ -0,0 +1,30 @@
+package org.example.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static String driver = "com.mysql.cj.jdbc.Driver";
+    private static String url = "jdbc:mysql://localhost:3306/bookingsystem";
+    private static String username = "root";
+    private static String password = "";
+    private static Connection conn;
+
+    public Connection getConnection() {
+        if (conn == null) {
+            try{
+                try{
+                    Class.forName(driver);
+                    conn = DriverManager.getConnection(url, username, password);
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return conn;
+    }
+}
