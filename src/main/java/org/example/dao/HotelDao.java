2d86da51e7b75b8c7e27267b466c1cc09c92e273
Jonas Andersen <hvidovrefifa@gmail.com>
2026-05-22 05:56:43 +0200

    refactor: HotelDao method to return Hotel object from Result and setParameterIndex to avoid redundancy.

--- src/main/java/org/example/dao/HotelDao.java
+++ src/main/java/org/example/dao/HotelDao.java
@@ -15,12 +15,7 @@ public class HotelDao {
 
         try(Connection con = DBConnection.getConnection()){
             PreparedStatement  ps = con.prepareStatement("INSERT INTO hotels(name, description, address, city, country, stars) VALUES(?,?,?,?,?,?)");
-            ps.setString(1, hotel.getName());
-            ps.setString(2, hotel.getDescription());
-            ps.setString(3, hotel.getAddress());
-            ps.setString(4, hotel.getCity());
-            ps.setString(5, hotel.getCountry());
-            ps.setInt(6, hotel.getStars());
+            setParameterIndex(ps,hotel);
 
             return ps.executeUpdate() > 0;
 
@@ -33,12 +28,7 @@ public class HotelDao {
 
         try(Connection con = DBConnection.getConnection()){
             PreparedStatement ps = con.prepareStatement("UPDATE hotels SET name = ?, description = ?, address = ?, city = ?, country = ?, stars = ?, updated_at = ?, created_at = ? WHERE id = ?");
-            ps.setString(1, hotel.getName());
-            ps.setString(2, hotel.getDescription());
-            ps.setString(3, hotel.getAddress());
-            ps.setString(4, hotel.getCity());
-            ps.setString(5, hotel.getCountry());
-            ps.setInt(6, hotel.getStars());
+            setParameterIndex(ps,hotel);
             ps.setTimestamp(7, Timestamp.valueOf(hotel.getUpdatedAt()));
             ps.setTimestamp(8, Timestamp.valueOf(hotel.getCreatedAt()));
             ps.setInt(9, hotel.getId());
@@ -61,16 +51,7 @@ public class HotelDao {
             ResultSet rs = ps.executeQuery();
 
             if(rs.next()){
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String address = rs.getString("address");
-                String city = rs.getString("city");
-                String country = rs.getString("country");
-                int stars = rs.getInt("stars");
-                String description = rs.getString("description");
-                LocalDateTime createdAt = rs.getTimestamp("created_at").toLocalDateTime();
-                LocalDateTime updatedAt = rs.getTimestamp("updated_at").toLocalDateTime();
-                hotel = new Hotel(id,name,address,city,country,stars,description,createdAt,updatedAt);
+                hotel = getHotelFromResultSet(rs);
             }
 
 
@@ -91,16 +72,7 @@ public class HotelDao {
 
             while(rs.next()) {
 
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String address = rs.getString("address");
-                String city = rs.getString("city");
-                String country = rs.getString("country");
-                int stars = rs.getInt("stars");
-                String description = rs.getString("description");
-                LocalDateTime createdAt = rs.getTimestamp("created_at").toLocalDateTime();
-                LocalDateTime updatedAt = rs.getTimestamp("updated_at").toLocalDateTime();
-                Hotel hotel = new Hotel(id, name, address, city, country, stars, description, createdAt, updatedAt);
+                Hotel hotel = getHotelFromResultSet(rs);
                 hotels.add(hotel);
             }
 
@@ -108,6 +80,37 @@ public class HotelDao {
             throw new RuntimeException(e);
         }
         return hotels;
+    }
+
+    private void setParameterIndex(PreparedStatement ps, Hotel hotel){
+        try {
+            ps.setString(1, hotel.getName());
+            ps.setString(2, hotel.getDescription());
+            ps.setString(3, hotel.getAddress());
+            ps.setString(4, hotel.getCity());
+            ps.setString(5, hotel.getCountry());
+            ps.setInt(6, hotel.getStars());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private Hotel getHotelFromResultSet(ResultSet rs){
+        try{
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            String address = rs.getString("address");
+            String city = rs.getString("city");
+            String country = rs.getString("country");
+            int stars = rs.getInt("stars");
+            String description = rs.getString("description");
+            LocalDateTime createdAt = rs.getTimestamp("created_at").toLocalDateTime();
+            LocalDateTime updatedAt = rs.getTimestamp("updated_at").toLocalDateTime();
+           return   new Hotel(id, name, address, city, country, stars, description, createdAt, updatedAt);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
